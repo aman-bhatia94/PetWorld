5ed1d243128156bbfa7e691fc53cd3df4dfcf0b4
Aman Bhatia <amanbhatia@Amans-MacBook-Pro.local>
2020-02-23 21:12:55 -0800

    Changed login,register,added strings in strings.xml

--- app/src/main/java/com/ateam/petworld/activities/RegisterActivity.java
+++ app/src/main/java/com/ateam/petworld/activities/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.ateam.petworld.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.ateam.petworld.R;
 
@@ -12,5 +13,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void onlocationCheckBoxClick(View view) {
     }
 }
