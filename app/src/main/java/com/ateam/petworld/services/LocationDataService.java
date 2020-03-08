7e71f5141e6f68d9066dfeeb7b9d480ea258b74c
vaishakhiification <vaishakhipilankar@gmail.com>
2020-03-07 16:23:36 -0800

    Added locationDataService changes
    Signed-off-by: vaishakhiification <vaishakhipilankar@gmail.com>

--- app/src/main/java/com/ateam/petworld/services/LocationDataService.java
+++ app/src/main/java/com/ateam/petworld/services/LocationDataService.java
@@ -4,16 +4,12 @@ import android.util.Log;
 
 import com.amazonaws.amplify.generated.graphql.CreateLocationMutation;
 import com.amazonaws.amplify.generated.graphql.GetLocationQuery;
-import com.amazonaws.amplify.generated.graphql.GetOwnerQuery;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
 import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
 import com.apollographql.apollo.GraphQLCall;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 import com.ateam.petworld.models.Location;
-import com.ateam.petworld.models.Owner;
-
-import java.util.Objects;
 
 import javax.annotation.Nonnull;
 
@@ -30,8 +26,8 @@ public class LocationDataService {
         CreateLocationInput requestObj = CreateLocationInput.builder()
                 .id(location.getId())
                 .displayName(location.getDisplayName())
-                .displayPlace("")
-                .displayAddress("")
+                .displayPlace(location.getDisplayPlace())
+                .displayAddress(location.getDisplayAddress())
                 .latitude(String.valueOf(location.getLatitude()))
                 .longitude(String.valueOf(location.getLongitude()))
                 .build();
@@ -39,8 +35,8 @@ public class LocationDataService {
                 new GraphQLCall.Callback<CreateLocationMutation.Data>() {
                     @Override
                     public void onResponse(@Nonnull Response<CreateLocationMutation.Data> response) {
-                        assert response.data() != null;
-                        Log.i("Results", "Added Todo" + Objects.requireNonNull(response.data().createLocation()).id());
+                        String id = response.data() != null && response.data().createLocation() != null ? response.data().createLocation().id() : null;
+                        Log.i("Results", "Added Todo" + id);
                     }
 
                     @Override
@@ -51,42 +47,42 @@ public class LocationDataService {
         );
     }
 
-    public Location getLocationById(String locationId) {
+    public Location getLocationById(Location location) {
         Location responseData = new Location();
-        awsAppSyncClient.query(GetLocationQuery.builder().id(locationId).build())
+        awsAppSyncClient.query(GetLocationQuery.builder().id(location.getId()).build())
                 .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
                 .enqueue(new GraphQLCall.Callback<GetLocationQuery.Data>() {
-                    @Override
-                    public void onResponse(@Nonnull Response<GetLocationQuery.Data> response) {
+                             @Override
+                             public void onResponse(@Nonnull Response<GetLocationQuery.Data> response) {
 
-                        GetLocationQuery.GetLocation locationQueryResponse = response.data() != null ? response.data().getLocation() : null;
-                        if (locationQueryResponse == null)
-                            return;
+                                 GetLocationQuery.GetLocation locationQueryResponse = response.data() != null ? response.data().getLocation() : null;
+                                 if (locationQueryResponse == null)
+                                     return;
 
-                        responseData.setId(locationQueryResponse.id());
-                        responseData.setDisplayAddress(locationQueryResponse.displayAddress());
-                        responseData.setCity(locationQueryResponse.city());
-                        responseData.setCountry(locationQueryResponse.country());
-                        responseData.setCountryCode(locationQueryResponse.countryCode());
-                        responseData.setDisplayName(locationQueryResponse.displayName());
-                        responseData.setHouseNumber(locationQueryResponse.houseNo());
-                        responseData.setLatitude(Double.parseDouble(locationQueryResponse.latitude()));
-                        responseData.setLongitude(Double.parseDouble(locationQueryResponse.longitude()));
-                        responseData.setDisplayPlace(locationQueryResponse.displayPlace());
-                        responseData.setName(locationQueryResponse.name());
-                        responseData.setNeighbourhood(locationQueryResponse.neighbourhood());
-                        responseData.setPostcode(locationQueryResponse.postCode());
-                        responseData.setRoad(locationQueryResponse.road());
-                        responseData.setState(locationQueryResponse.state());
-                        responseData.setSuburb(locationQueryResponse.suburb());
+                                 responseData.setId(locationQueryResponse.id());
+                                 responseData.setDisplayAddress(locationQueryResponse.displayAddress());
+                                 responseData.setCity(locationQueryResponse.city());
+                                 responseData.setCountry(locationQueryResponse.country());
+                                 responseData.setCountryCode(locationQueryResponse.countryCode());
+                                 responseData.setDisplayName(locationQueryResponse.displayName());
+                                 responseData.setHouseNumber(locationQueryResponse.houseNo());
+                                 responseData.setLatitude(Double.parseDouble(locationQueryResponse.latitude()));
+                                 responseData.setLongitude(Double.parseDouble(locationQueryResponse.longitude()));
+                                 responseData.setDisplayPlace(locationQueryResponse.displayPlace());
+                                 responseData.setName(locationQueryResponse.name());
+                                 responseData.setNeighbourhood(locationQueryResponse.neighbourhood());
+                                 responseData.setPostcode(locationQueryResponse.postCode());
+                                 responseData.setRoad(locationQueryResponse.road());
+                                 responseData.setState(locationQueryResponse.state());
+                                 responseData.setSuburb(locationQueryResponse.suburb());
 
-                    }
+                             }
 
-                    @Override
-                    public void onFailure(@Nonnull ApolloException e) {
-                        Log.e("Error", e.toString());
-                    }
-                }
+                             @Override
+                             public void onFailure(@Nonnull ApolloException e) {
+                                 Log.e("Error", e.toString());
+                             }
+                         }
                 );
         return responseData;
     }
